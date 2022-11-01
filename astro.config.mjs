7e0df4dcdc8684449c1069c6bb44c00c6a47269a
Giulia Talamonti <giulia.talamonti@credimi.com>
2022-11-01 18:17:05 +0100

    adds gitaction deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://giuliacot.github.io',
+  base: '/lotteryGame',
 });
