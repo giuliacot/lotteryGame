9b86a2db82f328f1c0301f5e63517c014cef6095
Giulia Talamonti <giulia.talamonti@credimi.com>
2022-11-01 18:26:36 +0100

    fixes missing adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [react()],
   site: 'https://giuliacot.github.io',
   base: '/lotteryGame',
-});
+})
