9a67d6ccb69b650a07996469407c6fcb45b65dda
Giulia Talamonti <giulia.talamonti@credimi.com>
2022-11-03 22:38:38 +0100

    fixes

--- src/pages/players.ts
+++ src/pages/players.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function addPlayers(values: Record<string, unknown>) {
   try {
-    const result = await axios.post(
+    await axios.post(
       `https://sheet.best/api/sheets/b11dcb32-ec76-4542-8794-abfe56afb99f`,
       values
     )
