21a581d06f27a89dd6bee7df006d199c54d08a5a
Giulia Talamonti <giulia.talamonti@credimi.com>
2022-11-03 21:39:48 +0100

    updates deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import node from '@astrojs/node'
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
