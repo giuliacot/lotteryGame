5bb64a4a1d7fb79c4b7c5ed274e33782e3a290ee
Giulia Talamonti <giulia.talamonti@credimi.com>
2022-12-21 22:47:44 +0100

    sets reducer and lifecycle

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,34 +1,52 @@
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useEffect, useReducer, useState } from 'react'
 import { addPlayers } from '../../pages/players'
+import { InfoBox } from '../InfoBox.tsx/InfoBox'
+import { initReducer, reducer } from './reducer'
 
 export const Player = () => {
-  const [nickname, setNickname] = useState<FormDataEntryValue>()
-  const [done, setDone] = useState<boolean>(false)
+  const [state, dispatch] = useReducer(reducer, initReducer)
+
+  const setPlayers = async (nickname: string) => {
+    try {
+      const r = await addPlayers({
+        Partecipant: nickname,
+        Date: new Date(),
+      })
+      if (r && r.hasOwnProperty('data')) {
+        dispatch({ type: 'set_done' })
+      }
+    } catch (e) {
+      console.error(e)
+      dispatch({ type: 'set_error' })
+    }
+  }
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    // why errorboundary doensn't catch on event handler https://stackoverflow.com/questions/58809160/errorboundary-not-catching-error-thrown-by-imported-function
     event.preventDefault()
     const formPartecipant = Object.fromEntries(
       new FormData(event.currentTarget).entries()
     )
-    setNickname(formPartecipant.nickname)
+
+    if (typeof formPartecipant.nickname === 'string') {
+      setPlayers(formPartecipant.nickname)
+      dispatch({ type: 'set_loading' })
+    }
   }
 
-  useEffect(() => {
-    if (nickname && typeof nickname === 'string') {
-      const addsData = async () => {
-        await addPlayers({
-          Partecipant: nickname,
-          Date: new Date(),
-        })
-        setDone(true)
-      }
-      addsData()
-    }
-  }, [nickname])
+  if (state.error) {
+    return <InfoBox type="error">Oh no! I can't add your name :( </InfoBox>
+  }
 
-  return done ? (
-    <p>Thanks to partecipate!</p>
-  ) : (
+  if (state.done) {
+    return <InfoBox type="info">Well done and thanks to partecipate!</InfoBox>
+  }
+
+  if (state.loading) {
+    return <span>Loading...</span>
+  }
+
+  return (
     <form id="getNickname" onSubmit={handleSubmit}>
       <label>Your nickname</label>
       <input type="text" placeholder="Your nickname" name="nickname" />
