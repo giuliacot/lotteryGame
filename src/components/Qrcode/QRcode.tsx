cd8e1ad52126824eb4ba22c54a577f1695666418
Giulia Talamonti <giulia.talamonti@credimi.com>
2022-12-28 00:09:08 +0100

    updates style of qrcode

--- src/components/Qrcode/QRcode.tsx
+++ src/components/Qrcode/QRcode.tsx
@@ -1,17 +1,28 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import QRCodeLib from 'qrcode'
 
 export const QRCode: React.FunctionComponent = () => {
   let canvas = useRef(null)
 
+  const [qrCodeColor, setQrCodeColor] = useState<string>('')
+
+  useEffect(() => {
+    if (window) {
+      // Get the Value of a Custom Property
+      const styles = getComputedStyle(document.documentElement)
+      console.log(styles.getPropertyValue('--tart-orange'))
+      setQrCodeColor(styles.getPropertyValue('--tart-orange'))
+    }
+  }, [])
+
   useEffect(() => {
     QRCodeLib.toCanvas(canvas.current, `${window.location.origin}/player`, {
       scale: 10,
       color: {
-        dark: '#005dad',
+        dark: qrCodeColor,
       },
     })
-  }, [])
+  }, [qrCodeColor])
 
   return <canvas ref={canvas} />
 }
