2f2565bbc801cd66d16727fba5fa708476df119f
Giulia Talamonti <giulia.talamonti@credimi.com>
2022-12-28 17:59:18 +0100

    fixes custom properties

--- src/components/Qrcode/QRcode.tsx
+++ src/components/Qrcode/QRcode.tsx
@@ -4,14 +4,20 @@ import QRCodeLib from 'qrcode'
 export const QRCode: React.FunctionComponent = () => {
   let canvas = useRef(null)
 
-  const [qrCodeColor, setQrCodeColor] = useState<string>('')
+  const [qrCodeColor, setQrCodeColor] = useState<{
+    dark: string
+    light: string
+  } | null>(null)
 
   useEffect(() => {
     if (window) {
-      // Get the Value of a Custom Property
+      // Get the Value of a Custom Property from the root
       const styles = getComputedStyle(document.documentElement)
-      console.log(styles.getPropertyValue('--tart-orange'))
-      setQrCodeColor(styles.getPropertyValue('--tart-orange'))
+
+      setQrCodeColor({
+        dark: styles.getPropertyValue('--tart-orange').trim(),
+        light: styles.getPropertyValue('--ghost-white').trim(),
+      })
     }
   }, [])
 
@@ -19,7 +25,8 @@ export const QRCode: React.FunctionComponent = () => {
     QRCodeLib.toCanvas(canvas.current, `${window.location.origin}/player`, {
       scale: 10,
       color: {
-        dark: qrCodeColor,
+        dark: qrCodeColor?.dark,
+        light: qrCodeColor?.light,
       },
     })
   }, [qrCodeColor])
