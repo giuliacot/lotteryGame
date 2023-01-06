b0229684bff7345cbbd52af6b72cbcdad074b8dd
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-01-06 23:28:16 +0100

    experiments on 3d css properties

--- src/components/Poll/Poll.tsx
+++ src/components/Poll/Poll.tsx
@@ -1,6 +1,7 @@
 import react, {
   FunctionComponent,
   useEffect,
+  useMemo,
   useReducer,
   useState,
 } from 'react'
@@ -8,11 +9,19 @@ import { getPlayers } from '../../api/players'
 import { initReducer, reducer } from '../../utils/reducer'
 import { InfoBox } from '../InfoBox.tsx/InfoBox'
 
+import style from './Poll.module.scss'
+
 type Partecipants = { Partecipant: string; Date: string }[] | null
+
+function getRandomIntInclusive(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min) // The maximum is inclusive and the minimum is inclusive
+}
 
 export const Poll: FunctionComponent = () => {
   const [partecipants, setPartecipants] = useState<Partecipants>(null)
   const [state, dispatch] = useReducer(reducer, initReducer)
+
+  const [winner, setWinner] = useState<string | null>(null)
 
   const getData = async () => {
     try {
@@ -28,7 +37,17 @@ export const Poll: FunctionComponent = () => {
     }
   }
 
-  useEffect(() => {
+  useMemo(() => {
+    setTimeout(() => {
+      const randomNumber = getRandomIntInclusive(1, partecipants?.length ?? 1)
+      if (!winner) setWinner(partecipants?.at(randomNumber)?.Partecipant ?? '')
+    }, 2000)
+  }, [partecipants])
+
+  // study again setTimeout and setInterval
+  // state update is outside the component, and react sees those state variables and pass through the component the snapshot
+
+  useEffect(function fetchPartecipants() {
     if (!partecipants) {
       dispatch({ type: 'set_loading' })
       getData()
@@ -37,17 +56,39 @@ export const Poll: FunctionComponent = () => {
 
   return (
     <>
-      {state.loading && <span>Loading...</span>}
-      {state.error && (
-        <InfoBox type="error">
-          Oh no! I don't know who are the players :({' '}
-        </InfoBox>
-      )}
       <ul>
         {partecipants?.map((p, i) => {
           return <li key={i}>{p.Partecipant}</li>
         })}
       </ul>
+      {/* Experiments with */}
+      {/* <div className={style.slotMachineWrapper}>
+        <div className={style.slotMachineDisplay}>
+          <div className={style.slotMachineScreen} />
+        </div>
+        <div className={style.slotMachineKeyboard}>
+          <div className={style.slotMachineKeyboard__left}></div>
+          <div className={style.slotMachineKeyboard__right}></div>
+        </div>
+        <div className={style.slotMachineBorder}></div>
+      </div> */}
+      <h1 className="hGrande">List of partecipants</h1>
+      <section className={style.slotMachine}>
+        <div className={style.slotMachineDisplay}></div>
+        <div className={style.slotMachineKeyboard}>
+          <div className={style.slotMachineKeyboardFront}></div>
+          <div className={style.slotMachineKeyboardBotton}></div>
+          <div className={style.slotMachineKeyboardLeft}></div>
+          <div className={style.slotMachineKeyboardRight}></div>
+        </div>
+      </section>
+      <p>{winner}</p>
+      {state.loading && <span>Loading...</span>}
+      {state.error && (
+        <InfoBox type="error">
+          Oh no! I don't know who the players are 😵‍💫{' '}
+        </InfoBox>
+      )}
     </>
   )
 }
