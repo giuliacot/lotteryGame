5d97615ad79e29f9c0e0e1ddcc0f1f584cfc4927
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-01-17 17:19:54 +0100

    configures ssr

---
+++ src/pages/players.ts
@@ -0,0 +1,28 @@
+type PlayerRecord = { fields: { nickname: string } }
+
+export async function post({ request }: { request: Request }) {
+  const data = await request.json()
+  const addPlayerUrl = `https://api.airtable.com/v0/${
+    import.meta.env.AIRTABLE_BASE_ID
+  }/${import.meta.env.AIRTABLE_TABLE_ID}`
+
+  const fields: PlayerRecord = { fields: { ...data } }
+
+  const response = await fetch(addPlayerUrl, {
+    method: 'POST',
+    headers: {
+      authorization: `Bearer ${import.meta.env.AIRTABLE_API_SECRET}`,
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(fields),
+  })
+
+  const responseData = await response.json()
+
+  if (responseData.error)
+    return new Response(JSON.stringify(responseData), { status: 500 })
+
+  return new Response(JSON.stringify(responseData), { status: 200 })
+}
+
+// TODO: adds get methods for Poll
