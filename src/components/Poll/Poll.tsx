dbce252124b5f818916d355a7b46bc82b855e7c8
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-01-27 12:27:42 +0100

    adds lever and pull animation

--- src/components/Poll/Poll.tsx
+++ src/components/Poll/Poll.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useReducer, useState } from 'react'
 import { initReducer, reducer } from '../../utils/reducer'
+import { Button } from '../Button/Button'
 import { InfoBox } from '../InfoBox.tsx/InfoBox'
 
 import style from './Poll.module.scss'
@@ -14,15 +15,25 @@ export const Poll = ({ names }: { names: any }) => {
   const [partecipants, setPartecipants] = useState<Partecipants>(names)
   const [state, dispatch] = useReducer(reducer, initReducer)
 
-  console.log(names)
   const [winner, setWinner] = useState<string | null>(null)
+  const [animationOn, setAnimationToggle] = useState(false)
 
-  useMemo(() => {
+  const PULL_LEVER_DURATION = 1000
+
+  const triggerAnimation = () => {
+    setAnimationToggle(true)
+    setTimeout(() => {
+      setAnimationToggle(false)
+    }, PULL_LEVER_DURATION)
+  }
+
+  const handlePoll = () => {
+    triggerAnimation()
     setTimeout(() => {
       const randomNumber = getRandomIntInclusive(1, partecipants?.length ?? 1)
-      if (!winner) setWinner(partecipants?.at(randomNumber)?.nickname ?? '')
+      setWinner(partecipants?.at(randomNumber)?.nickname ?? '')
     }, 2000)
-  }, [partecipants])
+  }
 
   // study again setTimeout and setInterval
   // state update is outside the component, and react sees those state variables and pass through the component the snapshot
@@ -41,21 +52,34 @@ export const Poll = ({ names }: { names: any }) => {
         <div className={style.slotMachineBorder}></div>
       </div> */}
       <h1 className="hGrande">List of partecipants</h1>
+
       <div>
         <section className={style.slotMachineWrap}>
           <div className={style.slotMachineDisplay}>
-            <div className={style.slotMachineLever} />
+            <div className={style.slotMachineLever}>
+              <div className={style.slotMachineLeverHandle}>
+                <div
+                  className={
+                    animationOn
+                      ? `${style.slotMachineLeverStick} ${style.animationLever}`
+                      : `${style.slotMachineLeverStick}`
+                  }
+                />
+              </div>
+            </div>
           </div>
-          <div className={style.slotMachineScreen} />
+          <div className={style.slotMachineScreen}>
+            <p className={style.slotMachineWinner}>{winner}</p>
+          </div>
           <div className={style.slotMachineKeyboard}>
-            <div className={style.slotMachineKeyboardFront}></div>
-            <div className={style.slotMachineKeyboardBotton}></div>
-            <div className={style.slotMachineKeyboardLeft}></div>
-            <div className={style.slotMachineKeyboardRight}></div>
+            <div className={style.slotMachineKeyboardFront} />
+            <div className={style.slotMachineKeyboardBotton} />
+            <div className={style.slotMachineKeyboardLeft} />
+            <div className={style.slotMachineKeyboardRight} />
           </div>
         </section>
       </div>
-      <p>{winner}</p>
+      <Button onClick={() => handlePoll()}>Start raffle off ✨</Button>
     </>
   )
 }
