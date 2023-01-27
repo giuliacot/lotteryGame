2a21f75b941823600f198cb39004304e2025ee52
Giulia Talamonti <giulia.talamonti@credimi.com>
2023-01-27 12:27:32 +0100

    fixes button props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,20 @@
 import style from './Button.module.scss'
 import type { ReactElement } from 'react'
 
+interface ButtonSpecificProps extends React.HTMLAttributes<HTMLButtonElement> {
+  type?: 'submit'
+  form?: string
+  children?: ReactElement | string
+}
+
 export const Button = ({
   type,
   form,
   children,
-}: {
-  type: 'submit'
-  form: string
-  children?: ReactElement | string
-}) => {
+  ...props
+}: ButtonSpecificProps) => {
   return (
-    <button className={style.button} type={type} form={form}>
+    <button className={style.button} type={type} form={form} {...props}>
       {children}
     </button>
   )
