cb66c5e520b5d08b0f7d979de0fd71fcd9654b90
Giulia Talamonti <giulia.talamonti@infinitaslearning.com>
2025-04-13 20:02:00 +0200

    updates Player

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,80 +1,86 @@
-import { FormEvent, useReducer } from 'react'
-import { InfoBox } from '../InfoBox.tsx/InfoBox'
-import { initReducer, reducer } from '../../utils/reducer'
-import { InputWithLabel } from '../InputWithLabel/InputWithLabel'
-import { Button } from '../Button/Button'
+import { useReducer } from "react";
+import { InfoBox } from "../InfoBox.tsx/InfoBox";
+import { initReducer, reducer } from "../../utils/reducer";
+import { InputWithLabel } from "../InputWithLabel/InputWithLabel";
+import { Button } from "../Button/Button";
 
-import style from './Player.module.scss'
+import style from "./Player.module.scss";
 
 export const Player = () => {
-  const [state, dispatch] = useReducer(reducer, initReducer)
+	const [state, dispatch] = useReducer(reducer, initReducer);
 
-  const setPlayers = async (nickname: string) => {
-    try {
-      const response = await fetch('/players', {
-        method: 'POST',
-        body: JSON.stringify({
-          nickname,
-        }),
-      })
+	const setPlayers = async (nickname: string) => {
+		try {
+			const response = await fetch("/players", {
+				method: "POST",
+				body: JSON.stringify({
+					nickname,
+				}),
+			});
 
-      const addedPlayer = await response.json()
+			console.log(response);
 
-      if (addedPlayer) {
-        dispatch({ type: 'set_done' })
-      }
-    } catch (e) {
-      console.error(e)
-      dispatch({ type: 'set_error' })
-    }
-  }
+			const addedPlayer = await response.json();
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    // why errorboundary doensn't catch on event handler https://stackoverflow.com/questions/58809160/errorboundary-not-catching-error-thrown-by-imported-function
-    event.preventDefault()
-    const formPartecipant = Object.fromEntries(
-      new FormData(event.currentTarget).entries()
-    )
+			if (addedPlayer) {
+				dispatch({ type: "set_done" });
+			}
+		} catch (e) {
+			console.error(e);
+			dispatch({ type: "set_error" });
+		}
+	};
 
-    if (typeof formPartecipant.nickname === 'string') {
-      setPlayers(formPartecipant.nickname)
-      dispatch({ type: 'set_loading' })
-    }
-  }
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		// why errorboundary doesn't catch on event handler https://stackoverflow.com/questions/58809160/errorboundary-not-catching-error-thrown-by-imported-function
+		event.preventDefault();
+		console.log(event.currentTarget);
+		const formParticipant = Object.fromEntries(
+			new FormData(event.currentTarget).entries(),
+		);
 
-  if (state.done) {
-    return <InfoBox type="info">Well done and thanks to partecipate!</InfoBox>
-  }
+		if (typeof formParticipant.nickname === "string") {
+			setPlayers(formParticipant.nickname);
+			dispatch({ type: "set_loading" });
+		}
+	};
 
-  return (
-    <div>
-      <h1 className="hGrande">Insert your funniest nickname</h1>
-      {state.loading ? (
-        <span>Loading...</span>
-      ) : (
-        <form
-          className={style.nicknameForm}
-          id="getNickname"
-          onSubmit={handleSubmit}
-        >
-          <InputWithLabel.Label labelFor="nickname">
-            Nickname
-          </InputWithLabel.Label>
-          <InputWithLabel.Input
-            id="nickname"
-            placeholder="Your nickname"
-            name="nickname"
-          />
+	if (state.done) {
+		return <InfoBox type="info">Well done and thanks to participate!</InfoBox>;
+	}
 
-          <Button type="submit" form="getNickname">
-            Let's play!
-          </Button>
-        </form>
-      )}
+	return (
+		<div className={style.playerInsertWrapper}>
+			<h1 className="hPiccolo">
+				Give us your name to be extracted for a terrific gift!
+			</h1>
+			{state.loading ? (
+				<span>Loading...</span>
+			) : (
+				<form
+					className={style.nicknameForm}
+					id="getNickname"
+					onSubmit={handleSubmit}
+				>
+					<InputWithLabel.Label labelFor="nickname">
+						Nickname
+					</InputWithLabel.Label>
+					<InputWithLabel.Input
+						id="nickname"
+						placeholder="A funny nickname of yours"
+						name="nickname"
+						className="input"
+					/>
 
-      {state.error && (
-        <InfoBox type="error">Oh no! I can't add your name :( </InfoBox>
-      )}
-    </div>
-  )
-}
+					<Button type="submit" form="getNickname" className={style.playerBtn}>
+						Let's play!
+					</Button>
+				</form>
+			)}
+
+			{state.error && (
+				<InfoBox type="error">Oh no! I can't add your name :( </InfoBox>
+			)}
+		</div>
+	);
+};
