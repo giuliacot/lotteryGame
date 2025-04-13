7975fb3950bdd44955b3d762cc88071ee9344377
Giulia Talamonti <giulia.talamonti@infinitaslearning.com>
2025-04-13 20:01:15 +0200

    updates input

--- src/components/InputWithLabel/InputWithLabel.tsx
+++ src/components/InputWithLabel/InputWithLabel.tsx
@@ -1,34 +1,34 @@
-import type React from 'react'
-import type { ReactElement } from 'react'
-import style from './InputWithLabel.module.scss'
+import type { ReactElement } from "react";
+import style from "./InputWithLabel.module.scss";
 
-type LabelType = { children: ReactElement | string; labelFor: string }
+type LabelType = { children: ReactElement | string; labelFor: string };
 type InputType = {
-  id?: string
-  placeholder?: string
-  name: string
-}
+	id?: string;
+	placeholder?: string;
+	name: string;
+	className?: string;
+};
 
-const Input = ({ placeholder, name }: InputType) => {
-  return (
-    <input
-      className={style.input}
-      type="text"
-      placeholder={placeholder}
-      name={name}
-    />
-  )
-}
+const Input = ({ placeholder, name, className }: InputType) => {
+	return (
+		<input
+			className={`${style.input} ${className}`}
+			type="text"
+			placeholder={placeholder}
+			name={name}
+		/>
+	);
+};
 
 const Label = ({ children, labelFor }: LabelType) => {
-  return (
-    <label className={style.label} htmlFor={labelFor}>
-      {children}
-    </label>
-  )
-}
+	return (
+		<label className={style.label} htmlFor={labelFor}>
+			{children}
+		</label>
+	);
+};
 
 export const InputWithLabel = {
-  Label,
-  Input,
-}
+	Label,
+	Input,
+};
