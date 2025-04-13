dfb04e3115766b8ce0841286a3bdb44342aa8800
Giulia Talamonti <giulia.talamonti@infinitaslearning.com>
2025-04-13 20:03:31 +0200

    updates Poll

--- src/components/Poll/Poll.tsx
+++ src/components/Poll/Poll.tsx
@@ -4,15 +4,16 @@ import { Button } from '../Button/Button'
 import { InfoBox } from '../InfoBox.tsx/InfoBox'
 
 import style from './Poll.module.scss'
+import PollLoadingSpinner from '../Loading/PollLoadingSpinner'
 
-type Partecipants = { creation_timestamp: string; nickname: string }[] | null
+type Participants = { creation_timestamp: string; nickname: string }[] | null
 
 function getRandomIntInclusive(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min) // The maximum is inclusive and the minimum is inclusive
 }
 
 export const Poll = ({ names }: { names: any }) => {
-  const [partecipants, setPartecipants] = useState<Partecipants>(names)
+  const [participants, setParticipants] = useState<Participants>(names)
   const [state, dispatch] = useReducer(reducer, initReducer)
 
   const [winner, setWinner] = useState<string | null>(null)
@@ -24,14 +25,15 @@ export const Poll = ({ names }: { names: any }) => {
     setAnimationToggle(true)
     setTimeout(() => {
       setAnimationToggle(false)
+      setWinner(null) // reset old winner
     }, PULL_LEVER_DURATION)
   }
 
   const handlePoll = () => {
     triggerAnimation()
     setTimeout(() => {
-      const randomNumber = getRandomIntInclusive(1, partecipants?.length ?? 1)
-      setWinner(partecipants?.at(randomNumber)?.nickname ?? '')
+      const randomNumber = getRandomIntInclusive(1, participants?.length ?? 1)
+      setWinner(participants?.at(randomNumber)?.nickname ?? '')
     }, 2000)
   }
 
@@ -51,9 +53,9 @@ export const Poll = ({ names }: { names: any }) => {
         </div>
         <div className={style.slotMachineBorder}></div>
       </div> */}
-      <h1 className="hGrande">List of partecipants</h1>
 
-      <div>
+      <div className={style.main}>
+        <h1 className="hGrande">Who is the next frontend buddy?</h1>
         <section className={style.slotMachineWrap}>
           <div className={style.slotMachineDisplay}>
             <div className={style.slotMachineLever}>
@@ -69,17 +71,32 @@ export const Poll = ({ names }: { names: any }) => {
             </div>
           </div>
           <div className={style.slotMachineScreen}>
-            <p className={style.slotMachineWinner}>{winner}</p>
+            <p className={style.slotMachineWinner}>
+              {animationOn && <PollLoadingSpinner />}
+              {winner && !animationOn && (
+                <span
+                  className={`${
+                    !animationOn
+                      ? style.slotMachineWinnerName
+                      : style.hideWinnerPlayer
+                  }`}
+                >
+                  {winner}
+                </span>
+              )}
+            </p>
           </div>
           <div className={style.slotMachineKeyboard}>
             <div className={style.slotMachineKeyboardFront} />
-            <div className={style.slotMachineKeyboardBotton} />
+            <div className={style.slotMachineKeyboardButton} />
             <div className={style.slotMachineKeyboardLeft} />
             <div className={style.slotMachineKeyboardRight} />
           </div>
         </section>
+        <Button className={style.drawTheWinner} onClick={() => handlePoll()}>
+          Draw the next frontend buddy ✨
+        </Button>
       </div>
-      <Button onClick={() => handlePoll()}>Start raffle off ✨</Button>
     </>
   )
 }
